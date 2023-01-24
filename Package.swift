3961eaa463afe09541a23654f2128c7fdea4b016
Sri Chand Ganguri <srichand@uvik.in>
2023-01-24 17:50:41 +0530

    initial commit

--- Package.swift
+++ Package.swift
@@ -6,23 +6,31 @@ import PackageDescription
 let package = Package(
     name: "MyStaticPackage",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyStaticPackage",
-            targets: ["MyStaticPackage"]),
+            targets: ["MyStaticTarget"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire", "5.4.0"..."5.6.4"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MyStaticPackage",
-            dependencies: []),
-        .testTarget(
-            name: "MyStaticPackageTests",
-            dependencies: ["MyStaticPackage"]),
+            name: "MyStaticTarget",
+            dependencies: [
+                .target(name: "MyStaticWrapper")
+            ],
+            path: "./Sources/MyStaticTarget"
+        ),
+        .target(
+            name: "MyStaticWrapper",
+            dependencies: [
+                .target(name: "MyStaticLib"),
+                .product(name: "Alamofire", package: "Alamofire"),
+            ],
+            path: "./Sources/MyStaticWrapper"
+        ),
+        .binaryTarget(
+            name: "MyStaticLib",
+            path: "./Sources/MyStaticPackage/MyStaticLib.xcframework")
     ]
 )
